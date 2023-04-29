2df15df2233507368d2c3a1d47ce58d436d9a837
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 08:01:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+ let num2 = 31;
+ let num3 = 44;
+ let num4 =  8;
+
+ let multiply = num1 * num2
+
+ let random = Math.floor(Math.random() *10) + 1;
+
+ let  mod = num3 % num4
+ let max = Math.max (2,5,20)
